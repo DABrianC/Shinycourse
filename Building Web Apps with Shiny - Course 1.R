43681d7f23eea3ac1b82baf70e3a093acc8dbe10
DABrianC <DABrianC@users.noreply.github.com>
2024-05-07 16:57:08 -0400

    BC

--- Building Web Apps with Shiny - Course 1.R
+++ Building Web Apps with Shiny - Course 1.R
@@ -3,7 +3,7 @@
 #Chapter 1----
 library(shiny)
 
-#exercise 1
+#exercise 1 - create an input text box
 ui <- fluidPage(
   #makes an input box for name. The box is called User1
   textInput("name", "User1:"),
@@ -16,7 +16,7 @@ server <- function(input, output, session) {
   output$greeting <- renderText({
     #This makes text appear under the input box that says "Hello," 
     #and the text that is typed into the box
-    paste("Hello,", input$name)
+    paste("Hello,", input$name) #this pulls the name from the input box
   })
 }
 
@@ -30,7 +30,7 @@ library(babynames)
 ui <- fluidPage(
   titlePanel("Baby Name Explorer")
   ,sidebarLayout(sidebarPanel(
-    textInput('name', 'Enter Name', 'David')
+    textInput(inputId = 'name', label = 'Enter Name', 'David')
   )
   # CODE BELOW: Display the plot output named 'trend'
   , 
@@ -49,4 +49,120 @@ server <- function(input, output, session) {
 
 shinyApp(ui = ui, server = server)
 
+#exercise 3 - babynames app continued
+ui <- fluidPage(
+  titlePanel("What's in a Name?"),
+  # CODE BELOW: Add select input named "sex" to choose between "M" and "F"
+  selectInput("sex"
+              , "Select Sex"
+              , selected = "F"
+              , choices = c("M", "F")),
+  #Add slider input named 'year' to select years  (1900 - 2010)
+  sliderInput("year"
+              , "Select Year"
+              , value = 1900
+              , min = 1900
+              , max = 2010),
+  # Add plot output to display top 10 most popular names
+  plotOutput('plot_top_10_names'),
+  #Add table output named "table_top_10_names"
+  tableOutput("table_top_10_names")
+)
 
+server <- function(input, output, session){
+  # Render plot of top 10 most popular names
+  output$plot_top_10_names <- renderPlot({
+    # Get top 10 names by sex and year
+    top_10_names <- babynames %>% 
+      # MODIFY CODE BELOW: Filter for the selected sex
+      #filter for the slider year
+      filter(sex == input$sex) %>% 
+      filter(year == input$year) %>% 
+      slice_max(prop, n = 10)
+    # Plot top 10 names by sex and year
+    ggplot(top_10_names, aes(x = name, y = prop)) +
+      geom_col(fill = "#263e63")
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+#exercise 3 - babynames app continued - table not plot
+library(DT)
+ui <- fluidPage(
+  titlePanel("What's in a Name?"),
+  # CODE BELOW: Add select input named "sex" to choose between "M" and "F"
+  selectInput("sex"
+              , "Select Sex"
+              , selected = "F"
+              , choices = c("M", "F")),
+  #Add slider input named 'year' to select years  (1900 - 2010)
+  sliderInput("year"
+              , "Select Year"
+              , value = 1900
+              , min = 1900
+              , max = 2010),
+  #Add table output named "table_top_10_names" and make it interactive using DT
+  DT::DTOutput("table_top_10_names")
+)
+
+server <- function(input, output, session){
+    # Get top 10 names by sex and year
+    top_10_names <- function() {
+      babynames %>% 
+      # MODIFY CODE BELOW: Filter for the selected sex
+      #filter for the slider year
+      filter(sex == input$sex) %>% 
+      filter(year == input$year) %>% 
+      slice_max(prop, n = 10)
+    }
+    #Render a table output named "table_top_10_names"
+    output$table_top_10_names <- DT::renderDT({
+      top_10_names()
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
+#exercise 4 - adding plotly to the babynames stuff
+library(plotly)
+ui <- fluidPage(
+  selectInput('name', 'Select Name', top_10_names$name),
+  # CODE BELOW: Add a plotly output named 'plot_trendy_names'
+  plotly::plotlyOutput("plotly_trendy_names"),
+  #Add table output named "table_top_10_names" and make it interactive using DT
+  DT::DTOutput("table_top_10_names")
+  
+)
+
+server <- function(input, output, session){
+  # Get top 10 names by sex and year
+  top_10_names <- function() {
+    babynames %>% 
+      # MODIFY CODE BELOW: Filter for the selected sex
+      #filter for the slider year
+      filter(sex == input$sex) %>% 
+      filter(year == input$year) %>% 
+      slice_max(prop, n = 10)
+  }
+  
+  #Render a table output named "table_top_10_names"
+  output$table_top_10_names <- DT::renderDT({
+    top_10_names()
+  })
+  # Function to plot trends in a name
+  plot_trends <- function(){
+    babynames %>% 
+      filter(name == input$name) %>% 
+      ggplot(aes(x = year, y = n)) +
+      geom_col()
+  }
+  # CODE BELOW: Render a plotly output named 'plot_trendy_names'
+  output$plotly_trendy_names <- plotly::renderPlotly({
+    plot_trends
+  })
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
